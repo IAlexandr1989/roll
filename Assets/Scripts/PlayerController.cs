6cfae727fa8d8252e910cb6162ad4d140b5b7b90
developer <developer1989@bk.ru>
2015-08-05 00:47:35 +0300

    initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace roll
+{
+	public class PlayerController : MonoBehaviour {
+		public float speed;
+		private Rigidbody rb;
+		private int count;
+		public int targets;
+		public Text targetText;
+		public Text winText;
+		public Text levelText;
+		public int level ;
+
+		IEnumerator restarting() {
+			Debug.Log("Before Waiting 2 seconds");
+			yield return new WaitForSeconds(2);
+			Application.LoadLevel(Application.loadedLevel);
+			Debug.Log("After Waiting 2 Seconds");
+		}
+		void Start () {
+			if (level == 1) {
+				//PlayerPrefs.SetInt("Player level", level);
+			}
+			int lvl = PlayerPrefs.GetInt("Player level");
+			if (lvl != 0) {
+				level = lvl;
+			}
+			rb = GetComponent<Rigidbody>();
+			targets = level;
+			finishCheck ();
+			levelText.text = "Level: " + level.ToString();
+		}
+		void FixedUpdate () {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			rb.AddForce (movement * speed);
+		}
+		
+		void OnTriggerEnter (Collider other) {
+			if (other.gameObject.CompareTag("Pick Up")) {
+				other.gameObject.SetActive(false);
+				count = count + 1;
+				finishCheck();
+			}
+			if (other.gameObject.CompareTag ("Boundary")) {
+				if (count != targets) {
+					winText.text = "Restarting..";
+					StartCoroutine(restarting());
+				}
+			}
+		}
+
+		void finishCheck () {
+			targetText.text = "Targets: " + (targets - count).ToString();
+			if (count == targets) { 
+				winText.text = "You Win. Next round!";
+				level = level + 1;
+				PlayerPrefs.SetInt ("Player level", level);
+				StartCoroutine(restarting());
+			}
+		}
+
+		public void resetLevel () {
+			PlayerPrefs.SetInt ("Player level", 1);
+			winText.text = "Level reset..";
+			StartCoroutine(restarting());
+		}
+	}
+}
---
+++ Assets/Scripts/WallGenerator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace roll
+{
+	public class WallGenerator : MonoBehaviour {
+		public Transform[] tilePrefabs;
+		public Transform PickUp;
+		public int sections;
+		// Use this for initialization
+		void Start () {
+			GameObject pl = GameObject.FindGameObjectWithTag ("Player");
+			PlayerController plc = pl.GetComponent<PlayerController> ();
+			sections = plc.targets;
+			GenerateLevel();
+		}
+
+		public void GenerateLevel() {
+			Transform currentTile = null;
+			Transform previousTile = null;
+			Transform puClone = null;
+			for (int i = 0; i < sections; i++) {
+				currentTile = Instantiate(tilePrefabs[Random.Range(0, tilePrefabs.Length)], transform.position, Quaternion.identity) as Transform; // tilePrefabs[Random.Range(0, tilePrefabs.Length)].rotation
+
+				if(previousTile) {
+					Transform offset = previousTile.Find("End");
+
+					if(offset) {
+						int rr = Random.Range(0, 2);
+						if (currentTile.tag.ToString() == "_I") {
+							rr = rr + 1;
+						}
+						currentTile.rotation = Quaternion.Euler(0, 90 * rr, 0);//Random.rotation;//offset.rotation;
+						currentTile.position += offset.position - currentTile.Find("Start").position;
+					}
+				}
+				
+				previousTile = currentTile;
+				Transform endOffset = previousTile.Find ("End");
+				Vector3 puPosition = new Vector3(endOffset.position.x, (float)(1.3), endOffset.position.z);
+				puClone = Instantiate(PickUp, puPosition, Quaternion.identity) as Transform;
+			}
+		}
+	}
+}
